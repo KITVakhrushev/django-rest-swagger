1ed19a6515cea6d2084159fedde2fdff85933d7d
andrey <a.vakhrushev@kit-xxi.com.ua>
2017-07-12 14:37:51 +0300

    set timeout

--- rest_framework_swagger/static/rest_framework_swagger/init.js
+++ rest_framework_swagger/static/rest_framework_swagger/init.js
@@ -1,61 +1,66 @@
 $(function () {
-  hljs.configure({
-    highlightSizeThreshold: 5000
-  });
+    hljs.configure({
+        highlightSizeThreshold: 5000
+    });
 
-  // Pre load translate...
-  if(window.SwaggerTranslator) {
-    window.SwaggerTranslator.translate();
-  }
-  var settings = {
-    url: window.location.pathname + '?format=openapi',
-    dom_id: "swagger-ui-container",
-    onComplete: function(swaggerApi, swaggerUi){
-      if(typeof initOAuth == "function") {
-        initOAuth({
-          clientId: "your-client-id",
-          clientSecret: "your-client-secret-if-required",
-          realm: "your-realms",
-          appName: "your-app-name",
-          scopeSeparator: ",",
-          additionalQueryStringParams: {}
-        });
-      }
-
-      if(window.SwaggerTranslator) {
+    // Pre load translate...
+    if (window.SwaggerTranslator) {
         window.SwaggerTranslator.translate();
-      }
-      addCsrfTokenHeaders();
-    },
-    onFailure: function(data) {
-      log("Unable to Load SwaggerUI");
-    },
-  };
-  $.extend(settings, JSON.parse($('#drs-settings').html()));
-
-  window.swaggerUi = new SwaggerUi(settings);
-
-  window.swaggerUi.load();
-  window.swaggerUi.api.setSchemes(['https']);
-
-  function addCsrfTokenHeaders() {
-    var token = $('[name="csrfmiddlewaretoken"]')[0];
-    if (!token) {
-      return;
     }
-    swaggerUi.api.clientAuthorizations.add(
-      'csrf_token',
-      new SwaggerClient.ApiKeyAuthorization(
-        'X-CSRFToken',
-        token.value,
-        'header'
-      )
-    );
-  }
+    var settings = {
+        url: window.location.pathname + '?format=openapi',
+        dom_id: "swagger-ui-container",
+        onComplete: function (swaggerApi, swaggerUi) {
+            if (typeof initOAuth == "function") {
+                initOAuth({
+                    clientId: "your-client-id",
+                    clientSecret: "your-client-secret-if-required",
+                    realm: "your-realms",
+                    appName: "your-app-name",
+                    scopeSeparator: ",",
+                    additionalQueryStringParams: {}
+                });
+            }
 
-  function log() {
-    if ('console' in window) {
-      console.log.apply(console, arguments);
+            if (window.SwaggerTranslator) {
+                window.SwaggerTranslator.translate();
+            }
+            addCsrfTokenHeaders();
+        },
+        onFailure: function (data) {
+            log("Unable to Load SwaggerUI");
+        },
+    };
+    $.extend(settings, JSON.parse($('#drs-settings').html()));
+
+    window.swaggerUi = new SwaggerUi(settings);
+
+    window.swaggerUi.load();
+    $('body').hide();
+
+    setTimeout(function () {
+        window.swaggerUi.api.setSchemes(['https']);
+        $('body').show();
+    }, 5000);
+
+    function addCsrfTokenHeaders() {
+        var token = $('[name="csrfmiddlewaretoken"]')[0];
+        if (!token) {
+            return;
+        }
+        swaggerUi.api.clientAuthorizations.add(
+            'csrf_token',
+            new SwaggerClient.ApiKeyAuthorization(
+                'X-CSRFToken',
+                token.value,
+                'header'
+            )
+        );
     }
-  }
+
+    function log() {
+        if ('console' in window) {
+            console.log.apply(console, arguments);
+        }
+    }
 });
